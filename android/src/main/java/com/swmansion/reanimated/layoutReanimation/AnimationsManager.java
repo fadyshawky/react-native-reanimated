2334876d6f83b02f703512e46c350d8de907c7f8
FadyShawky <fadytshawky@gmail.com>
2024-03-03 15:02:21 +0200

    :bug: fix for onCatalystInstanceDestroy deprecated API

--- android/src/main/java/com/swmansion/reanimated/layoutReanimation/AnimationsManager.java
+++ android/src/main/java/com/swmansion/reanimated/layoutReanimation/AnimationsManager.java
@@ -67,7 +67,7 @@ public class AnimationsManager implements ViewHierarchyObserver {
     mSharedTransitionManager = new SharedTransitionManager(this);
   }
 
-  public void onCatalystInstanceDestroy() {
+  public void invalidate() {
     isCatalystInstanceDestroyed = true;
     mNativeMethodsHolder = null;
     mContext = null;
